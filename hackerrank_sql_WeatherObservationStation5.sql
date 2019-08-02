13d9f4e23da84c4cb8ad9de8534d502dd199bb22
IDEA JOON <49594612+ideajoon@users.noreply.github.com>
2019-08-03 02:13:43 +0900

    Update hackerrank_sql_WeatherObservationStation5.sql

--- hackerrank_sql_WeatherObservationStation5.sql
+++ hackerrank_sql_WeatherObservationStation5.sql
@@ -1,25 +1,25 @@
-Query the two cities in STATION with the shortest and longest CITY names, 
-as well as their respective lengths (i.e.: number of characters in the name). 
-If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+-- Query the two cities in STATION with the shortest and longest CITY names, 
+-- as well as their respective lengths (i.e.: number of characters in the name). 
+-- If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+
 
 -- CASE1 OK
 
--- SELECT CITY, LENGTH(CITY)
---     FROM (SELECT CITY, LENGTH(CITY) AS LENG
---             FROM STATION
---             ORDER BY LENG, CITY)
---     WHERE ROWNUM <= 1
--- UNION
--- SELECT CITY, LENGTH(CITY)
---     FROM (SELECT CITY, LENGTH(CITY) AS LENG
---             FROM STATION
---             ORDER BY LENG DESC, CITY)
---     WHERE ROWNUM <= 1;
+SELECT CITY, LENGTH(CITY)
+    FROM (SELECT CITY, LENGTH(CITY) AS LENG
+            FROM STATION
+            ORDER BY LENG, CITY)
+    WHERE ROWNUM <= 1
+UNION
+SELECT CITY, LENGTH(CITY)
+    FROM (SELECT CITY, LENGTH(CITY) AS LENG
+            FROM STATION
+            ORDER BY LENG DESC, CITY)
+    WHERE ROWNUM <= 1;
 
 
 
-
-CASE2 OK
+-- CASE2 OK
 
 SELECT CITY, LENGTH(CITY)
     FROM (SELECT CITY,
